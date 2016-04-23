a0792b507f75f88723ee2c6780ef2e70013234da
Khan <khan4019@gmail.com>
2016-04-23 15:33:18 -0500

    added grid here

---
+++ grid/fakeData.js
@@ -0,0 +1,128 @@
+var fakeData = [
+  {
+    "Name": "Laverne Willis",
+    "Gender": "female",
+    "Company": "GRONK",
+    "Phone": "(996) 552-3331",
+    "Address": "726 Jay Street, Hendersonville, Alabama, 2081",
+    "Salary": "$91,135.94",
+    "FavoriteLibrary": "meteor"
+  },
+  {
+    "Name": "Mabel Dorsey",
+    "Gender": "female",
+    "Company": "EZENT",
+    "Phone": "(962) 554-2416",
+    "Address": "763 Creamer Street, Brandywine, New York, 5416",
+    "Salary": "$67,076.81",
+    "FavoriteLibrary": "jquery"
+  },
+  {
+    "Name": "Myra Hicks",
+    "Gender": "female",
+    "Company": "OVATION",
+    "Phone": "(970) 420-3880",
+    "Address": "315 Hanover Place, Falconaire, Tennessee, 9993",
+    "Salary": "$56,036.67",
+    "FavoriteLibrary": "angular"
+  },
+  {
+    "Name": "Clayton Odom",
+    "Gender": "male",
+    "Company": "ZILLACTIC",
+    "Phone": "(902) 483-3121",
+    "Address": "297 Catherine Street, Tryon, Maine, 7282",
+    "Salary": "$51,761.25",
+    "FavoriteLibrary": "angular"
+  },
+  {
+    "Name": "Elvia Johnston",
+    "Gender": "female",
+    "Company": "ECSTASIA",
+    "Phone": "(825) 530-2387",
+    "Address": "887 Bergen Court, Marenisco, North Dakota, 2088",
+    "Salary": "$62,850.67",
+    "FavoriteLibrary": "ember"
+  },
+  {
+    "Name": "Dorothea Mathews",
+    "Gender": "female",
+    "Company": "ZIPAK",
+    "Phone": "(950) 512-3174",
+    "Address": "565 Blake Court, Shasta, Idaho, 5460",
+    "Salary": "$90,964.99",
+    "FavoriteLibrary": "meteor"
+  },
+  {
+    "Name": "Mia Golden",
+    "Gender": "female",
+    "Company": "CRUSTATIA",
+    "Phone": "(829) 597-3540",
+    "Address": "637 Colin Place, Hollins, California, 3461",
+    "Salary": "$86,585.23",
+    "FavoriteLibrary": "ember"
+  },
+  {
+    "Name": "Daugherty Haynes",
+    "Gender": "male",
+    "Company": "CAXT",
+    "Phone": "(843) 498-2388",
+    "Address": "734 Victor Road, Camas, West Virginia, 6465",
+    "Salary": "$91,284.30",
+    "FavoriteLibrary": "Extjs"
+  },
+  {
+    "Name": "Lewis Mann",
+    "Gender": "male",
+    "Company": "BIOHAB",
+    "Phone": "(806) 569-2585",
+    "Address": "631 Radde Place, Byrnedale, Michigan, 9628",
+    "Salary": "$61,843.22",
+    "FavoriteLibrary": "angular"
+  },
+  {
+    "Name": "Marie Leonard",
+    "Gender": "female",
+    "Company": "DENTREX",
+    "Phone": "(908) 440-3228",
+    "Address": "970 Frank Court, Gila, Minnesota, 5987",
+    "Salary": "$77,885.88",
+    "FavoriteLibrary": "ember"
+  },
+  {
+    "Name": "Sears Underwood",
+    "Gender": "male",
+    "Company": "DADABASE",
+    "Phone": "(977) 455-3671",
+    "Address": "219 Tampa Court, Crumpler, Marshall Islands, 1899",
+    "Salary": "$98,778.19",
+    "FavoriteLibrary": "backbone"
+  },
+  {
+    "Name": "Yolanda Alvarez",
+    "Gender": "female",
+    "Company": "ZOID",
+    "Phone": "(893) 507-2765",
+    "Address": "568 Haring Street, Callaghan, Mississippi, 9937",
+    "Salary": "$52,126.99",
+    "FavoriteLibrary": "meteor"
+  },
+  {
+    "Name": "Leah Collier",
+    "Gender": "female",
+    "Company": "ISOTRONIC",
+    "Phone": "(824) 596-2805",
+    "Address": "602 Albee Square, Weogufka, Utah, 6040",
+    "Salary": "$77,006.65",
+    "FavoriteLibrary": "Extjs"
+  },
+  {
+    "Name": "Roxie Winters",
+    "Gender": "female",
+    "Company": "DECRATEX",
+    "Phone": "(952) 555-2405",
+    "Address": "736 Bliss Terrace, Rossmore, Pennsylvania, 8511",
+    "Salary": "$99,806.24",
+    "FavoriteLibrary": "ember"
+  }
+]
